3bb985ac5d9a27003aebcebc0ffd9e808df2ee72
Jakob Egger <jakob@eggerapps.at>
2023-10-25 10:45:05 +0200

    Initial Commit

---
+++ OpenWebsiteBlocker/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  OpenWebsiteBlocker
+//
+//  Created by Jakob Egger on 25.10.23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
