7866a4beeb95ddd392c66621a4ccdc483e8754bb
Jakob Egger <jakob@eggerapps.at>
2023-10-25 15:32:13 +0200

    Implemented blocked domains using user defaults

--- OpenWebsiteBlocker Extension/Resources/background.js
+++ OpenWebsiteBlocker Extension/Resources/background.js
@@ -1,6 +1,43 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+function updateRules() {
+	browser.runtime.sendNativeMessage("application.id", "BlockedDomains", async function(blockedDomains) {
+		console.log("Received sendNativeMessage response:");
+		console.log(blockedDomains);
+		
+		
+		var oldRules = await browser.declarativeNetRequest.getDynamicRules()
+		
+		for (oldRule of oldRules) {
+			console.log("removing dynamic rules " + oldRule);
+			await browser.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [ oldRule.id ] });
+		}
+		
+		console.log("removing dynamic rules done!");
+		
+		for (i=0;i<blockedDomains.length;i++) {
+			console.log("adding dynamic rule for " + blockedDomains[i]);
+			var rule = {
+				id: 1 + i,
+				priority: 1,
+				action: { type: "block" },
+				condition: {
+					resourceTypes: [ "main_frame" ],
+					urlFilter: "||" + blockedDomains[i]
+				}
+			}
+			console.log(rule);
+			await browser.declarativeNetRequest.updateDynamicRules({ addRules: [ rule ] });
+		}
+		
+		console.log("updateDynamicRules addRules done!");
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
+	});
+
+}
+
+let port = browser.runtime.connectNative("application.id");
+port.onMessage.addListener(function(message) {
+	console.log("Received native port message:");
+	console.log(message);
 });
+
+updateRules()
--- OpenWebsiteBlocker Extension/SafariWebExtensionHandler.swift
+++ OpenWebsiteBlocker Extension/SafariWebExtensionHandler.swift
@@ -8,6 +8,7 @@
 import SafariServices
 import os.log
 
+
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
@@ -27,10 +28,22 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
             message = request?.userInfo?["message"]
         }
 
-        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@ (profile: %@)", String(describing: message), profile?.uuidString ?? "none")
+		let response = NSExtensionItem()
 
-        let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "echo": message ] ]
+		if message as? String == "BlockedDomains" {
+			if let sharedDefaults = UserDefaults(suiteName: "at.eggerapps.OpenWebsiteBlocker.Shared") {
+				if let blockedDomains = sharedDefaults.stringArray(forKey: "BlockedDomains") {
+					os_log(.default, "Extension requested BlockedDomains, returning: \(blockedDomains, privacy: .public)")
+					response.userInfo = [ SFExtensionMessageKey: blockedDomains ]
+				} else {
+					os_log(.error, "No blocked domains configured")
+				}
+			} else {
+				os_log(.error, "Can't access shared defaults")
+			}
+		} else {
+			os_log(.error, "Received invalid message from extension: \(String(describing:message), privacy: .public)")
+		}
 
         context.completeRequest(returningItems: [ response ], completionHandler: nil)
     }
